e9fccc149fdbd614707e52f008dd90b479d6a9d5
Your Name <lloydabell4@gmail.com>
2021-10-27 19:59:10 -0400

    update

--- class notes.R
+++ class notes.R
@@ -19,5 +19,6 @@ ad_aov <- aov(mmse ~ sex * drug_treatment * health_status, data = sum_df)
 summary(ad_aov)
 
 tidyTUKEY <- TukeyHSD(ad_aov, which = 'drug_treatment:health_status',conf.level=0.95)
-
 plot(tidyTUKEY)
+tidyTUKEY <- tidy(tidyTUKEY)
+
